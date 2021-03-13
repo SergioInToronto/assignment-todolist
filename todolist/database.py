31a683a97539b5128aa57fde98b988b149d8edd4
Sergio Martins <sergio.martins@example.com>
2021-03-13 15:25:45 -0500

    Add Delete. update() throws 404 if item didn't exist

--- todolist/database.py
+++ todolist/database.py
@@ -7,10 +7,29 @@
 db = {}
 
 
+# A full application may want db, internal logic, and views to have their own error types.
+# For brevity, my db throws web-server-specific errors.
+from werkzeug import exceptions
+
+
 def list():
     return db
+
+
+def get(todo_id):
+    try:
+        return db[todo_id]
+    except KeyError:
+        raise exceptions.NotFound(f'todo item with ID "{todo_id}" does not exist.')
 
 
 def save(todo_id, todo):
     db[todo_id] = todo
     return todo
+
+
+def delete(todo_id):
+    try:
+        del db[todo_id]
+    except KeyError:
+        raise exceptions.NotFound(f'todo item with ID "{todo_id}" does not exist.')
--- todolist/server.py
+++ todolist/server.py
@@ -11,6 +11,7 @@ def build():
     app.add_url_rule("/api/todos", view_func=list_todos, methods=["GET"])
     app.add_url_rule("/api/todos", view_func=create_todo, methods=["POST"])
     app.add_url_rule("/api/todos/<todo_id>", view_func=update_todo, methods=["PUT"])
+    app.add_url_rule("/api/todos/<todo_id>", view_func=delete_todo, methods=["DELETE"])
     return app
 
 
@@ -27,5 +28,11 @@ def create_todo():
 
 @view_decorators.check_schema("todo_create_or_update")
 def update_todo(todo_id):
+    database.get()  # throws 404 if not found
     response = database.save(todo_id, flask.request.get_json())
+    return response
+
+
+def delete_todo(todo_id):
+    response = database.delete(todo_id)
     return (response, 201)
